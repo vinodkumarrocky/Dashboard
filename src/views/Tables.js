e1d20bd4dcfb5b3204333ab5d50f4a303596cae8
Vinod Kumar <vinodrocky504@gmail.com>
2019-06-13 20:50:50 +0530

    init

---
+++ src/views/Tables.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Table,
+  Row,
+  Col
+} from "reactstrap";
+
+class Tables extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="content">
+          
+        </div>
+      </>
+    );
+  }
+}
+
+export default Tables;
---
+++ src/views/wallet.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+// reactstrap components
+import { Card, CardHeader, CardBody, CardTitle, Row, Col } from "reactstrap";
+
+class wallet extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="content">
+          <Row>
+            
+          </Row>
+        </div>
+      </>
+    );
+  }
+}
+
+export default wallet;
