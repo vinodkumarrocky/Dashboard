8a5f1fe5c239abd0949f434722e26d278c87867f
Vinod Kumar <vinodrocky504@gmail.com>
2019-06-13 21:47:25 +0530

    Updates

---
+++ precache-manifest.533121b7cf0d524ec6099b4ae9feb190.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a914c4669403a7094f93b7607c55b88",
+    "url": "/dashboard-react/index.html"
+  },
+  {
+    "revision": "0d18e7cd221812d81d41",
+    "url": "/dashboard-react/static/css/2.654f473f.chunk.css"
+  },
+  {
+    "revision": "db777b7c8b71af46dbd6",
+    "url": "/dashboard-react/static/css/main.1243c49b.chunk.css"
+  },
+  {
+    "revision": "0d18e7cd221812d81d41",
+    "url": "/dashboard-react/static/js/2.d5014aa2.chunk.js"
+  },
+  {
+    "revision": "db777b7c8b71af46dbd6",
+    "url": "/dashboard-react/static/js/main.e12c7e1a.chunk.js"
+  },
+  {
+    "revision": "3ae4fe84485785068ab9",
+    "url": "/dashboard-react/static/js/runtime~main.c708546a.js"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/dashboard-react/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/dashboard-react/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "a6f24e7dd5470a8bd2f48ebb53dbe8d5",
+    "url": "/dashboard-react/static/media/damir-bosnjak.a6f24e7d.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/dashboard-react/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dashboard-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/dashboard-react/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/dashboard-react/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/dashboard-react/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/dashboard-react/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/dashboard-react/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
